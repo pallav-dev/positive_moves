4399bed3ed0e1c6cc4eef27315302cf20504f3c1
Dev_pallav <developer.pallav@gmail.com>
2022-02-05 00:40:43 +0530

    third commit

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -108,3 +108,139 @@ particlesJS("particles-js", {
 },
 "retina_detect": true
 });
+
+const newYork = () =>{
+   document.getElementById("contact-details").innerHTML = `<div class="list-group shadow-sm h-75">
+   <div class="list-group-item list-group-item-action h-100">
+       <div class="text-center">
+           <div><i class="bi bi-building text-primary fs-1"></i></div>
+           <h5 class="text-secondary fw-bold mb-3">MEET US</h5>
+           <h6>222 Broadway,
+            New York, NY 10038,</h6>
+           <h5 class="fw-bold">United States of America</h5>
+           <a class="text-decoration-none fw-500" href="#">info@positivemoves.com</a>
+       </div>
+   </div>
+</div>`;
+}
+
+const london = () =>{
+   document.getElementById("contact-details").innerHTML = `<div class="list-group shadow-sm h-75">
+   <div class="list-group-item list-group-item-action h-100">
+       <div class="text-center">
+           <div><i class="bi bi-building text-primary fs-1"></i></div>
+           <h5 class="text-secondary fw-bold mb-3">MEET US</h5>
+           <h6>2nd Floor, Berkeley Square House,
+            Berkeley Square, London W1J 6BD,</h6>
+           <h5 class="fw-bold">United Kingdom</h5>
+           <a class="text-decoration-none fw-500" href="#">info@positivemoves.com</a>
+       </div>
+   </div>
+</div>`;
+}
+
+const paris = () =>{
+   document.getElementById("contact-details").innerHTML = `<div class="list-group shadow-sm h-75">
+   <div class="list-group-item list-group-item-action h-100">
+       <div class="text-center">
+           <div><i class="bi bi-building text-primary fs-1"></i></div>
+           <h5 class="text-secondary fw-bold mb-3">MEET US</h5>
+           <h6>28 rue de l’Amiral Hamelin,
+            Paris, 75116,</h6>
+           <h5 class="fw-bold">France</h5>
+           <a class="text-decoration-none fw-500" href="#">info@positivemoves.com</a>
+       </div>
+   </div>
+</div>`;
+}
+
+const dubai = () =>{
+   document.getElementById("contact-details").innerHTML = `<div class="list-group shadow-sm h-75">
+   <div class="list-group-item list-group-item-action h-100">
+       <div class="text-center">
+           <div><i class="bi bi-building text-primary fs-1"></i></div>
+           <h5 class="text-secondary fw-bold mb-3">MEET US</h5>
+           <h6>Unit 303, South Block,
+            Emirates Financial Tower,DIFC,</h6>
+           <h5 class="fw-bold">Dubai, UAE</h5>
+           <a class="text-decoration-none fw-500" href="#">info@positivemoves.com</a>
+       </div>
+   </div>
+</div>`;
+}
+
+const gurugram = () =>{
+   document.getElementById("contact-details").innerHTML = `<div class="list-group shadow-sm h-75">
+   <div class="list-group-item list-group-item-action h-100">
+       <div class="text-center">
+           <div><i class="bi bi-building text-primary fs-1"></i></div>
+           <h5 class="text-secondary fw-bold mb-3">MEET US</h5>
+           <h6>One Horizon Center, DLF Phase V,
+            Golf Course Road, Sector 43,</h6>
+           <h5 class="fw-bold">Gurugram, India</h5>
+           <a class="text-decoration-none fw-500" href="#">info@positivemoves.com</a>
+       </div>
+   </div>
+</div>`;
+}
+
+const mumbai = () =>{
+   document.getElementById("contact-details").innerHTML = `<div class="list-group shadow-sm h-75">
+   <div class="list-group-item list-group-item-action h-100">
+       <div class="text-center">
+           <div><i class="bi bi-building text-primary fs-1"></i></div>
+           <h5 class="text-secondary fw-bold mb-3">MEET US</h5>
+           <h6>7th Floor, North Annex, Tower 2A,
+            One World Centre,
+            Senapati Bapat Marg, Lower Parel,</h6>
+           <h5 class="fw-bold">Mumbai, India</h5>
+           <a class="text-decoration-none fw-500" href="#">info@positivemoves.com</a>
+       </div>
+   </div>
+</div>`;
+}
+
+const jammu = () =>{
+   document.getElementById("contact-details").innerHTML = `<div class="list-group shadow-sm h-75">
+   <div class="list-group-item list-group-item-action h-100">
+       <div class="text-center">
+           <div><i class="bi bi-building text-primary fs-1"></i></div>
+           <h5 class="text-secondary fw-bold mb-3">MEET US</h5>
+           <h6>6, Jeevan Bhawan,
+            1st Floor, Narwal</h6>
+           <h5 class="fw-bold">Jammu, India</h5>
+           <a class="text-decoration-none fw-500" href="#">info@positivemoves.com</a>
+       </div>
+   </div>
+</div>`;
+}
+
+const singapore = () =>{
+   document.getElementById("contact-details").innerHTML = `<div class="list-group shadow-sm h-75">
+   <div class="list-group-item list-group-item-action h-100">
+       <div class="text-center">
+           <div><i class="bi bi-building text-primary fs-1"></i></div>
+           <h5 class="text-secondary fw-bold mb-3">MEET US</h5>
+           <h6>1 Fullerton Road,
+            #02-01 One Fullerton,</h6>
+           <h5 class="fw-bold">Singapore</h5>
+           <a class="text-decoration-none fw-500" href="#">info@positivemoves.com</a>
+       </div>
+   </div>
+</div>`;
+}
+
+const hongKong = () =>{
+   document.getElementById("contact-details").innerHTML = `<div class="list-group shadow-sm h-75">
+   <div class="list-group-item list-group-item-action h-100">
+       <div class="text-center">
+           <div><i class="bi bi-building text-primary fs-1"></i></div>
+           <h5 class="text-secondary fw-bold mb-3">MEET US</h5>
+           <h6>2003, 20/F Tower 5,
+            China Hong Kong City,</h6>
+           <h5 class="fw-bold">33, Canton Road Tsim Sha Tsui Kowloon</h5>
+           <a class="text-decoration-none fw-500" href="#">info@positivemoves.com</a>
+       </div>
+   </div>
+</div>`;
+}
